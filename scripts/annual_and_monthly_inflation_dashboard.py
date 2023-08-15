aa35fd875b2f94aadbde1554d2ac37b8277f3ede
Any-Arlene Niyubahwe <any-arlene.niyubahwe@curalie.com>
2023-08-15 17:21:38 +0200

    make changes to inflation rate dashboards

---
+++ scripts/annual_and_monthly_inflation_dashboard.py
@@ -0,0 +1,80 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+
+# Modify the file_path to point to the 'cleaned_data' folder
+file_path = os.path.join("cleaned_data", "cleaned_taux_d'inflation_juin_2023.csv")
+
+try:
+    # Extract month_year from the CSV filename
+    base_name = os.path.basename(file_path)  # Extracts "cleaned_taux_d'inflation_juin_2023.csv"
+    match = re.search(r"_(\w+_\d{4})", base_name)
+    if match:
+        month_year = match.group(1)  # e.g., "juin_2023"
+    else:
+        raise ValueError("Could not extract month and year from the filename!")
+
+    # Read the CSV file into a Pandas DataFrame
+    df = pd.read_csv(file_path)
+    
+    # Handle NaN values in the DataFrame by filling with 0
+    df.fillna(0, inplace=True)
+
+    # Initialize the figure with 2 subplots: one above the other
+    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(14, 16))
+
+    # Plotting monthly inflation rates on the first subplot
+    colors = plt.cm.viridis_r(np.linspace(0, 1, len(df.columns[1:-1])))
+    for idx, month in enumerate(df.columns[1:-1]):
+        axes[0].plot(df['Annee'], df[month], marker='o', label=month, color=colors[idx])
+    axes[0].set_title(f"Taux d'Inflation Mensuelle ({month_year})")
+    axes[0].set_xlabel("Annee")
+    axes[0].set_ylabel("% Annuel")
+    axes[0].grid(True, which='both', linestyle='--', linewidth=0.5)
+    axes[0].legend(loc="upper left")
+
+    # Adjust y-axis ticks for the first subplot
+    min_val = df[df.columns[1:-1]].values.min()
+    max_val = df[df.columns[1:-1]].values.max()
+    if min_val != max_val and not (np.isnan(min_val) or np.isnan(max_val) or np.isinf(min_val) or np.isinf(max_val)):
+        step_val = 1 if (max_val - min_val) < 5 else 5
+        axes[0].set_yticks(np.arange(min_val, max_val + step_val, step_val))
+
+    # Plotting the Moyenne Annuelle on the second subplot
+    axes[1].plot(df['Annee'], df['Moyenne Annuelle'], marker='o', color='red', linewidth=2.5)
+    axes[1].set_title(f"Moyenne Annuelle du Taux d'Inflation ({month_year})")
+    axes[1].set_xlabel("Annee")
+    axes[1].set_ylabel("% Moyenne Annuel")
+    axes[1].grid(True, which='both', linestyle='--', linewidth=0.5)
+
+    # Adjust y-axis ticks for the second subplot
+    min_val = df['Moyenne Annuelle'].min()
+    max_val = df['Moyenne Annuelle'].max()
+    if min_val != max_val and not (np.isnan(min_val) or np.isnan(max_val) or np.isinf(min_val) or np.isinf(max_val)):
+        step_val = 1 if (max_val - min_val) < 5 else 5
+        axes[1].set_yticks(np.arange(min_val, max_val + step_val, step_val))
+
+    # Adjust layout to prevent overlap
+    plt.tight_layout()
+
+    # Create a new folder (if it doesn't exist)
+    new_folder = "inflation_rate_dashboards"
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
+
+    # Save the figure into the new folder using month_year
+    save_path = os.path.join(new_folder, f"{month_year}_dashboard_1.png")
+    plt.savefig(save_path)
+
+    # Display the plot
+    #plt.show()
+
+# Catch specific file not found error
+except FileNotFoundError:
+    print(f"File {file_path} not found.")
+
+# Catch any other general exceptions/errors
+except Exception as e:
+    print(f"Error: {e}")
---
+++ scripts/monthly_inflation_dashboard.py
@@ -0,0 +1,64 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+
+# Adjust the file_path to point to the 'cleaned_data' folder
+file_path = os.path.join("cleaned_data", "cleaned_taux_d'inflation_juin_2023.csv")
+
+try:
+    # Extract month_year from the CSV filename
+    base_name = os.path.basename(file_path)
+    match = re.search(r"_(\w+_\d{4})", base_name)
+    if match:
+        month_year = match.group(1)
+    else:
+        raise ValueError("Could not extract month and year from the filename!")
+
+    # Read the CSV file into a Pandas DataFrame
+    df = pd.read_csv(file_path)
+
+    # Handle NaN values in the DataFrame
+    df.fillna(0, inplace=True)
+
+    # Number of rows and columns for subplots
+    n_rows = 4
+    n_cols = 3
+
+    # Create a figure and a set of subplots
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, 12))
+
+    # Flatten the 2D list of axes into 1D for easier indexing
+    axes_flat = axes.flatten()
+
+    # Loop through each month's column and plot it on its own subplot
+    for idx, month in enumerate(df.columns[1:-1]):
+        ax = axes_flat[idx]
+        ax.plot(df['Annee'], df[month], marker='o', color='blue')
+        ax.set_title(month)
+        ax.grid(True, which='both', linestyle='--', linewidth=0.5)
+        ax.set_xlabel("Annee")
+        ax.set_ylabel("% Annuel")
+
+    # Adjust the layout of the plots to prevent overlap
+    plt.tight_layout()
+    fig.subplots_adjust(top=0.92)  # Adjust the top space
+    fig.suptitle(f"Taux d'Inflation Mensuelle ({month_year})", fontsize=16)
+
+    # Create a new folder (if it doesn't exist) to save the generated dashboard
+    new_folder = "inflation_rate_dashboards"
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
+
+    # Save the figure into the new folder using month_year
+    save_path = os.path.join(new_folder, f"{month_year}_dashboard_2.png")
+    fig.savefig(save_path)
+
+    # Display the plots
+    #plt.show()
+
+except FileNotFoundError:
+    print(f"File {file_path} not found.")
+except Exception as e:
+    print(f"Error: {e}")
