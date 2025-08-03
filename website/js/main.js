d78a043fdeee2cc97683c6e2492211d8bea393da
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2025-08-03 02:50:14 +0200

    feat: Add BaseChart and ContinentImportsChart classes

---
+++ website/js/main.js
@@ -0,0 +1,8 @@
+import { ContinentImportsChart } from "./charts/ContinentImportsChart.js";
+
+// Initialize all charts when the document is ready
+document.addEventListener("DOMContentLoaded", async () => {
+  // Initialize the continent imports chart
+  const continentChart = new ContinentImportsChart("importChart");
+  await continentChart.initialize();
+});
